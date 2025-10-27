f78ff541ed35d816ee33ef35508a743ab9eba5a6
siddhidhamnaskar <siddhidhamnaskar64@gmail.com>
2025-10-27 08:50:45 +0530

    Force SSR mode for Amplify

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',       // important for Amplify SSR
-  trailingSlash: true,
-  env: {
-    DATABASE_URL:process.env.DATABAE_URL, // ensures TypeScript errors break the build
-    JWT_SECRET:process.env.JWT_CESRET
-  },
-     images: {
-    domains: ['fakestoreapi.com'],
-  },
+ 
  
 };
 
