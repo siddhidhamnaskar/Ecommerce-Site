abb913c2da06cdb3635e9efd1583396ada5cbf7b
siddhidhamnaskar <siddhidhamnaskar64@gmail.com>
2025-11-04 07:58:04 +0530

    product card image property changed

--- src/components/productCard.tsx
+++ src/components/productCard.tsx
@@ -29,7 +29,7 @@ export default function ProductCard({product}:ProductCardProps){
                     src={product.image}
                     alt={product.name}
                     fill
-                    className="object-cover"
+                    className="object-content"
                     sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                   />
                 </div>
